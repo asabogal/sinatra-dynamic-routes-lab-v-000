7105e5f45dd5969b947ce377b1991937cc7d0d3d
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-16 14:17:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,8 +8,8 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    @sq = params[:number]
-    "#{@sq * @sq}"
+    # @sq = params[:number]
+    # "#{@sq * @sq}"
   end
 
   get '/say/:number/:phrase' do
