758a8a809b070e59de171601a4c112c75f29a2e5
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-16 15:49:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @op = params[:op]
+    @op = params[:operation]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
     if @op == "add"
