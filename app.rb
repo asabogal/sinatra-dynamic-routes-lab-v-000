9f6c6b25c2bcd74a2b93e17b75f4c7367ea59fa8
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-16 15:39:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
     @op = params[:op]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
-    @op == "add" ? @num1 + @num2
+    @op == "add" ? @num1 + @num2 : nil
     @op == "substract" ? "#{@num1 - @num2}" : nil
     @op == "multiply" ? "#{@num1 * @num2}" : nil
     @op == "divide" ? "#{@num1 / @num2}" : nil
