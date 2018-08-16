5c178702c08d3ce17d207b027058b3b1f009a67a
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-16 15:08:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,10 +17,7 @@ class App < Sinatra::Base
     @num = params[:number].to_i
     @phrase = params[:phrase]
     @answer = ""
-    @num.times do
-      @answer += @phrase
-    end
-    @answer
+    @num.times {@phrase}
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
