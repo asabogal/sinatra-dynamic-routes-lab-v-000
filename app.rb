0eed523761d8a4fa9b1affaeadf3c1639689b7c1
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-16 14:32:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,8 +16,8 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
     @phrase = params[:phrase]
-    @num.times do
-      @phrase
+    @num.times {"#{@phrase}"}
+      
       binding.pry
     end
   end
